928b67334892b7df8cb2ab3ae7913ed244f381e6
Rob Freedy <robert@prefect.io>
2025-05-19 10:04:09 -0400

    Fixing context

--- flow.py
+++ flow.py
@@ -1,11 +1,12 @@
 from prefect import flow, get_run_logger
 from prefect.context import FlowRunContext
+import json
 
 @flow
 def my_flow():
     logger = get_run_logger()
     logger.info("Hello, world!")
-    logger.info(FlowRunContext.get())
+    logger.info(json.dumps(FlowRunContext.get()))
 
 if __name__ == "__main__":
     my_flow()
