834a1854353552fbc91656e1e9c1f4a2d4f1b684
Rob Freedy <robert@prefect.io>
2025-05-19 10:00:05 -0400

    Adding flow run context

--- flow.py
+++ flow.py
@@ -1,9 +1,11 @@
 from prefect import flow, get_run_logger
+from prefect.context import FlowRunContext
 
 @flow
 def my_flow():
     logger = get_run_logger()
     logger.info("Hello, world!")
+    logger.info(FlowRunContext.get())
 
 if __name__ == "__main__":
     my_flow()
